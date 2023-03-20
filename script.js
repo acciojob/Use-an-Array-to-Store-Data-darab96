f98ee973ca51abf54d10754dcb2931f5afeca1dc
darab96 <106982371+darab96@users.noreply.github.com>
2023-03-20 15:28:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
 let yourArray =  [
-	"Darab",
-	69,
-	true,
-	"Mehtab",
-	420
+	{
+		name1 : "Darab"
+	}
+	{
+		num1 : 69
+	}
+	{
+	bool : true
+	}
+	{
+	name2 : "Mehtab"
+	}
+	{
+		num2 : 420
+	}
 ]
 
-console.log(yourArray.length);
+console.log(yourArray);
